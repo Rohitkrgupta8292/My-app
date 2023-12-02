2576ace1ff2dca22870eedb8f3426bc8483e98a6
Rohit kr Gupta <rohitkrgupta8292@gmail.com>
2023-12-02 22:04:47 +0530

    javascript

---
+++ web_development/video_54/new.js
@@ -0,0 +1,5 @@
+console.log("Hey i am console")
+console.log("Hey i node am console")
+console.log("Hey i am console")
+
+document.title = "Hey i am good"
---
+++ web_development/video_54/script.js
@@ -0,0 +1,14 @@
+alert("hello world")
+
+console.log("Hey i am console")
+
+let a = prompt("Enter your number")
+
+console.log("your number is " + a)
+let isTrue = confirm("Are you sure you want to leave this page?")
+if (isTrue) {
+    console.log("computer is blasting")
+}
+else {
+    console.log("you are staying here")
+}
